1ec1f4ad9a8e7f7d3cb13d1d0910d887cae636dc
moonii <mhlee@crossent.com>
2021-08-12 02:39:49 +0000

    modified cce.sh

--- cce.sh
+++ cce.sh
@@ -1,5 +1,5 @@
 # modify src/uaa/dependencies.gradle
-sed -i "ext[\"mariadb.version\"] = \"2.3.0\"/ext[\"mariadb.version\"] = \"2.5.1\"" src/uaa/dependencies.gradle
+sed -i "`(grep -Rn mariadb.version src/uaa/dependencies.gradle | awk -F ":" '{ print $1 }')`s/2.3.0/2.5.1/" src/uaa/dependencies.gradle
 
 # replace files
 cp src/uaa-javas/BackwardsCompatibleDelegatingPasswordEncoder.java src/uaa/server/src/main/java/org/cloudfoundry/identity/uaa/util/beans/BackwardsCompatibleDelegatingPasswordEncoder.java
